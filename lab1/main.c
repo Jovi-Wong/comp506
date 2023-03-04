766a8ff604e91d2395f1c30eac8de5d8d834483f
Jovi Wong <1350386163@qq.com>
2023-03-03 21:11:41 -0600

    comment

--- lab1/main.c
+++ lab1/main.c
@@ -17,39 +17,39 @@ int yyerror (const char *s) {
 }
 
 int main (int argc, char* argv[]) {
-	if (argc == 2)
-	{
-		if (!strcmp("-h", argv[1]))
-		{
-			printf("-h : help\n");
-			return 0;
-		} 
-		else
-		{
-			if((access(argv[1], F_OK)) != -1)
-			{   
-        		FILE *fp;
-				fp = fopen(argv[1], "r");
-				yyin = fp;
-    		}
-			else
-			{
-    			printf("Cannot find file %s.\n", argv[1]);
-    			return 0;
-   			}		
-		}
-	} 
-	else
-	{
-		yyin = stdin;
-	}
+	// if (argc == 2)
+	// {
+	// 	if (!strcmp("-h", argv[1]))
+	// 	{
+	// 		printf("-h : help\n");
+	// 		return 0;
+	// 	} 
+	// 	else
+	// 	{
+	// 		if((access(argv[1], F_OK)) != -1)
+	// 		{   
+    //     		FILE *fp;
+	// 			fp = fopen(argv[1], "r");
+	// 			yyin = fp;
+    // 		}
+	// 		else
+	// 		{
+    // 			printf("Cannot find file %s.\n", argv[1]);
+    // 			return 0;
+   	// 		}		
+	// 	}
+	// } 
+	// else
+	// {
+	// 	yyin = stdin;
+	// }
 
 	yyparse();
 
-	if (errorCount == 0) {
-		printf("Parse Successfully.\n");
-	} else {
-		printf("Parser fails. Total number of errors: %d.\n", errorCount);
-	}
+	// if (errorCount == 0) {
+	// 	printf("Parse Successfully.\n");
+	// } else {
+	// 	printf("Parser fails. Total number of errors: %d.\n", errorCount);
+	// }
 	return 0;
 }
